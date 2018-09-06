868f61126d1d087983a7493d20f4a594a1faf191
uchihaitachi24 <manidharanupoju@gmail.com>
2018-09-06 09:13:16 +0530

    Clubbed two endpoints into one

--- app.py
+++ app.py
@@ -24,20 +24,24 @@ def post(post_id):
     # return f"Post : {post['title']}, content : \n\n{post['content']}"
     return render_template('post.html', post=post)
 
+'''
 
 @app.route('/post/form')
 def form():
-    return render_template('create.html')
+    return render_template('create.html') # clubbed this into single end point
+
+'''
 
 
-@app.route('/post/create')
+@app.route('/post/create', methods=['GET', 'POST'])
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
-    post_id = len(posts)
-    posts[post_id] = {'id': post_id, 'title' : title, 'content': content }
-
-    return redirect(url_for('post', post_id = post_id))
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id': post_id, 'title' : title, 'content': content }
+        return redirect(url_for('post', post_id = post_id))
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
